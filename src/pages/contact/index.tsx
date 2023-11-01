790fe83d6f0d080af0e3e53882f26538cafb0024
fairuzikbar <fairuz.ikbar.w@mail.ugm.ac.id>
2023-11-01 16:43:04 +0700

    update title

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <>
       <Head>
-      <title>fairuzikbar | About</title>
+      <title>fairuzikbar | Contact</title>
       <meta name="description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI." />
       <meta property="og:title" content="fii maarif personal website"/>
       <meta property="og:description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI."/>
